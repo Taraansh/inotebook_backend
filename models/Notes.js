39e950fbd4d0cac4b632a95ad40254b9c51920c7
Taraansh <taraanshnandwani123@gmail.com>
2023-07-11 12:53:45 +0530

    Added model for Notes

---
+++ models/Notes.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const NotesSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  // title: String, // String is shorthand for {type: String}
+  // author: String,
+  // body: String,
+  // comments: [{ body: String, date: Date }],
+  // date: { type: Date, default: Date.now },
+  // hidden: Boolean,
+  // meta: {
+  //   votes: Number,
+  //   favs: Number
+  // }
+});
+
+module.exports = mongoose.model("notes", NotesSchema);
