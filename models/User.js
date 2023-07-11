89ffefa87736c08a0a52e11720dbd0d1f7d255c9
Taraansh <taraanshnandwani123@gmail.com>
2023-07-11 12:53:36 +0530

    Added model for user

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  // title: String, // String is shorthand for {type: String}
+  // author: String,
+  // body: String,
+  // comments: [{ body: String, date: Date }],
+  // date: { type: Date, default: Date.now },
+  // hidden: Boolean,
+  // meta: {
+  //   votes: Number,
+  //   favs: Number
+  // }
+});
+
+module.exports = mongoose.model("user", UserSchema);
