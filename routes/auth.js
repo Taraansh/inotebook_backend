b13553c81dc99443128bb1ae0d25263097ff55ff
Taraansh <taraanshnandwani123@gmail.com>
2023-07-13 14:47:12 +0530

    Login route

--- routes/auth.js
+++ routes/auth.js
@@ -17,12 +17,15 @@ router.post(
       min: 5,
     }),
   ],
+
   async (req, res) => {
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
     try {
       // Check whether the user with same email exists already
       let user = await User.findOne({ email: req.body.email });
@@ -42,16 +45,58 @@ router.post(
       });
 
       const data = {
-        user:{
-          id : user.id
-        }
-      }
-      const authToken = jwt.sign(data, JWT_SECRET)
-      res.json({authToken})
-      // res.json(user);
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// Authenticate a User using POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
