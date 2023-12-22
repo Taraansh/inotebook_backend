ce1129ae5e520e61ad4ff406fcff395ff6b5f254
Taraansh <taraanshnandwani123@gmail.com>
2023-12-22 13:47:00 +0530

    Updated DB

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
-
-const connectToMongo=()=>{
-  mongoose.connect(mongoURI);
-  console.log("connected to mongo");
-}
+const connectToMongo = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://taraansh11:taraansh11@cluster0.8uveyye.mongodb.net/iNotebook?retryWrites=true&w=majority"
+      // "mongodb://127.0.0.1:27017/iNotebook"
+    );
+    console.log(`Connected to MongoDB database ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error in MngoDb: ${error}`);
+  }
+};
 
 module.exports = connectToMongo;
